44f85005a1ec30b2a2de51ed2eda19b69d23095a
huxiaoqian <huxiaoqian@theduapp.com>
2019-11-05 16:19:22 +0800

    update demo download url

--- app/src/main/java/com/huxq17/download/demo/MainActivity.java
+++ app/src/main/java/com/huxq17/download/demo/MainActivity.java
@@ -53,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 progressDialog.setProgress(0);
                 progressDialog.show();
-                Pump.newRequest("http://10.176.61.42:9999/spl.mp4")
+                Pump.newRequest(url2)
                         .listener(new DownloadListener() {
 
                             @Override
@@ -80,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                             }
                         })
                         //Optionally,Set whether to repeatedly download the downloaded file,default false.
-                        .forceReDownload(false)
+                        .forceReDownload(true)
                         //Optionally,Set how many threads are used when downloading,default 3.
                         .threadNum(3)
                         .setRetry(3, 200)
